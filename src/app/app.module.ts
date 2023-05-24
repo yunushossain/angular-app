a46f92616145aa5b439b28c3fc245fe185f9e1a9
smunazir <munazir220114@gmail.com>
2023-05-24 08:34:10 +0600

    add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EInfoComponent } from './e-info/e-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EInfoComponent
   ],
   imports: [
     BrowserModule,
